c22af6a3f1d148c8484bd56d481a43ff76c931bd
soyoung677 <lsy141@gmail.com>
2018-02-22 15:46:29 -0800

    initial version - split pages

---
+++ insert_data.sql
@@ -0,0 +1,7 @@
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("01", "State-Capital", "California", "Sacramento");
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("01", "State-Capital", "Nevada", "Carson City");
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("01", "State-Capital", "Washington", "Olympia");
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("02", "Nation-Capital", "South Korea", "Seoul");
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("02", "Nation-Capital", "Japan", "Tokyo");
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("02", "Nation-Capital", "England", "London");
+insert into table_quiz (quiz_type_code, quiz_type_desc, quiz, answer) values ("02", "Nation-Capital", "France", "Paris");
