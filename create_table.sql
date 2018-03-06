593e5b9c8fa3c8ea2fb507379618791d9c5726ff
soyoung677 <lsy141@gmail.com>
2018-03-05 16:57:00 -0800

    change sql files

--- create_table.sql
+++
@@ -1,9 +0,0 @@
-create table quiz (
-	id int AUTO_INCREMENT primary key,
-    quiz_type_code int,
-    quiz_type_desc varchar(32),
-    quiz varchar(64),
-    answer varchar(64),
-    created timestamp DEFAULT CURRENT_TIMESTAMP
-)
-
